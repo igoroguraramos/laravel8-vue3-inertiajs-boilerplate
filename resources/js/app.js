ff8e2b375253f06e0fe6160a081a3ae01dc86c12
Igor Ogura Ramos <igoroguraramos@hotmail.com>
2023-01-31 15:41:03 -0300

    Instalando Vue3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import { createApp } from 'vue';
+import Welcome from './components/Welcome.vue';
+const app = createApp({});
+
+//Registrando o componente Welcome localizado em resources/js/components/Welcome.vue
+app.component('Welcome', Welcome);
+
+// div de id = app localizada em resources/views/welcome.blade.php
+app.mount('#app');
+
 require('./bootstrap');
